d908310a9fb812925cbab331ab50b744153564be
lherna1371 <lherna1371@gmail.com>
2022-03-08 18:44:19 -0600

    updated with script.js file

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+
+
+
+// event listener for flip button 
+document.querySelector("#flip").addEventListener('click', function(e){
+    console.log('hello, flip button was clicked!');
+})
+
+
+// event listener for clear button
+document.querySelector("#clear").addEventListener('click', function(){
+    console.log("hello, clear button was clicked!!!");
+})
